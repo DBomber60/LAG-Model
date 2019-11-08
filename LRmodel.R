16db77823227c002d2988b8e29145ea305a0d5aa
David Reynolds <davidreynolds@Davids-MacBook-Air-2.local>
2019-11-08 15:39:14 -0500

    updated LR model

--- LRmodel.R
+++ LRmodel.R
@@ -1,4 +1,5 @@
-# bayesian logistic regression with multivariate normal prior or beta parameters
+# bayesian logistic regression with multivariate normal prior on beta parameters
+# fit using a random walk MH algorithm (normal proposal distribution)
 # Beta ~ N(mu_0, sigma); mu_0 set to 0 vector here; sigma is a scaled identity matrix
 
 
@@ -11,34 +12,30 @@ logpost = function(beta_curr, Y, X, c) {
   t(X %*% beta_curr) %*% Y - sum(log(1 + exp(X %*% beta_curr))) - .5/c * crossprod(beta_curr)
 }
 
-# simulate from multivariate normal
-
-
-# random walk model
-nIter = 10000
-lsig = 1
-
-# initialize parameters
-beta_curr = c(0,0)
-logpi_curr = logpost(beta_curr, dat = Y)
-
-# keep track of results here
-Res = array(NA, dim = c(nIter, 2))
-
-for (k in 1:nIter) {
-  beta_prop = beta_curr + exp(lsig) * rnorm(2)
-  logpi_prop = logpost(beta_prop, dat = Y)
-  acc = min(1, exp(logpi_prop - logpi_curr) )
-  if (runif(1) < acc) {
-    # set beta to the proposed beta
-    beta_curr = beta_prop
-    logpi_curr = logpi_prop
+bayesLR = function(Y, X, nIter, c, lsig) {
+  
+  p = dim(X)[2] # number of beta parameters
+  
+  # initialize parameters
+  beta_curr = rep(0, p) 
+  logpi_curr = logpost(beta_curr, Y, X, c)
+  
+  # keep track of results here
+  Res = array(NA, dim = c(nIter, p))
+  
+  for (k in 1:nIter) {
+    beta_prop = beta_curr + exp(lsig) * rnorm(p) # propose new beta vector from normal distribution
+    logpi_prop = logpost(beta_prop, Y, X, c)
+    acc = min(1, exp(logpi_prop - logpi_curr) )
+    if (runif(1) < acc) {
+      # set beta to the proposed beta
+      beta_curr = beta_prop
+      logpi_curr = logpi_prop
+    }
+    Res[k,] = beta_curr
   }
-  Res[k,] = beta_curr
+  return(Res)
 }
-Output = Res[2001:nIter,]
-hist(Output[,2])
-
 
 # test above with simulated data - two coefficients
 set.seed(1)
@@ -47,21 +44,12 @@ X = matrix(rnorm(nobs * 2), nrow = nobs, ncol = 2)
 beta_true = c(1,2)
 Y = rbinom(nobs, 1, prob = plogis(X %*% beta_true) )
 
-# prior parameters
-mu_0 = c(0,0)
-c = 1
+draws = bayesLR(Y, X, nIter = 10000, c = 1, lsig = 1)
+
+# visualize posterior draws
+hist(draws[1000:nIter,2])
 
 
 # test by fitting traditional logistic regression
 m0 = glm(Y ~ X, family = binomial)
 summary(m0)
-
-
-
-
-# sim from MVN
-mu=c(1,2)
-Sigma=matrix(c(1,0.9,0.9,1),2,2,byrow=T)
-d=length(mu)
-L=t(chol(Sigma)); Z=rnorm(d,0,1)
-X=mu+L%*%Z
